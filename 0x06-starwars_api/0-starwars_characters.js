ac95784f0f5cb0be604683150496bb78ed964161
johnayinde <lolaayinde@gmail.com>
2023-03-20 10:55:44 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,41 @@
 #!/usr/bin/node
 
-/**
- * Star wars api
- * Script that prints all characters of a Star Wars movie:
- */
-const request = require('request');
-const filmId = process.argv[2];
-if (!filmId || isNaN(filmId)) {
-  process.exit(1);
+// Import the 'request' library
+const request = require('request')
+
+// Define constant with the base URL of the Star Wars API
+const API_URL = 'https://swapi-api.alx-tools.com/api'
+
+// Check if the number of command line arguments is greater than 2
+if (process.argv.length > 2) {
+  // Make a request to the film resource for the specified film ID
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    // If an error occurred during the request, log the error
+    if (err) {
+      console.log(err)
+    }
+    // Get the characters URL from the film's response body
+    const charactersURL = JSON.parse(body).characters
+
+    // Create an array of Promises that resolve with the names of the characters
+    const charactersName = charactersURL.map(
+      (url) =>
+        new Promise((resolve, reject) => {
+          // Make a request to the character resource
+          request(url, (promiseErr, __, charactersReqBody) => {
+            // If an error occurred during the request, reject the Promise with the error
+            if (promiseErr) {
+              reject(promiseErr)
+            }
+            // Resolve the Promise with the name of the character
+            resolve(JSON.parse(charactersReqBody).name)
+          })
+        })
+    )
+
+    // Wait for all Promises to resolve and log the names of the characters, separated by new lines
+    Promise.all(charactersName)
+      .then((names) => console.log(names.join('\n')))
+      .catch((allErr) => console.log(allErr))
+  })
 }
-const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const respPromises = [];
-
-  const json = JSON.parse(body);
-  const characters = json.characters;
-
-  characters.forEach((character) => {
-    const url = character;
-    const promise = new Promise((resolve, reject) => {
-      request(url, (error, response, body) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        const json = JSON.parse(body);
-        resolve(json.name);
-      });
-    });
-    respPromises.push(promise);
-  });
-  Promise.all(respPromises).then((values) => {
-    values.forEach((value) => {
-      console.log(value);
-    });
-  }).catch((error) => {
-    console.log(error);
-  });
-});
